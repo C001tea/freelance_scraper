2d91ac07114aba3673bb523f849035fdeca65afb
C001tea <155453518+C001tea@users.noreply.github.com>
2025-09-18 20:35:52 +0300

    Update freelance_scraper.py

--- freelance_scraper.py
+++ freelance_scraper.py
@@ -1,4 +1,4 @@
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
 import asyncio
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -8,20 +8,23 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 from selenium.webdriver.chrome.service import Service
 from telegram import Bot
+
+
+
 TOKEN = "7800854300:AAFHc4dPf25BboyL0q0iCp6AK6fH6QLTyvU"
 users_id = ["717613461"]
-
+infor = ["scrap", "script", "pdf"]
 
 def scraping():
     service = Service("chromedriver.exe")
     options = webdriver.ChromeOptions()
+    #options.add_argument('--headless')
     driver = webdriver.Chrome(service=service, options=options)
     driver.implicitly_wait(7)
 
-    wait = WebDriverWait(driver, 15)
+    wait = WebDriverWait(driver, 4000)
     all_info = []
     driver.get("https://www.freelancer.com/login")
-    time.sleep(5)
     email = wait.until(expected_conditions.presence_of_element_located((By.ID, "emailOrUsernameInput")))
     email.send_keys("rekatartem@gmail.com")
     password = wait.until(expected_conditions.presence_of_element_located((By.ID, "passwordInput")))
@@ -29,8 +32,7 @@ def scraping():
 
     wait.until(expected_conditions.element_to_be_clickable((By.XPATH,
                                                     "//button[contains(text(), 'Log in')]"))).click()
-    wait.until(expected_conditions.element_to_be_clickable((By.XPATH,
-                                                        "//fl-icon[@label='Minimize Inbox']"))).click()
+
     wait.until(expected_conditions.element_to_be_clickable((By.XPATH,
                                                     "//button[contains(text(), 'No, thanks')]"))).click()
     wait.until(expected_conditions.element_to_be_clickable((By.CLASS_NAME, "ModalCloseButton"))).click()
@@ -38,8 +40,8 @@ def scraping():
     wait.until(expected_conditions.element_to_be_clickable((By.XPATH, "//button[contains(., Browse)]"))).click()
     wait.until(expected_conditions.element_to_be_clickable((By.XPATH,
                                                             "//app-browse-links-item[contains(., Projects)]"))).click()
-    a = 0
-    while a < 5:
+
+    while True:
         stop = find_element(all_info, driver)
         if stop:
             break
@@ -48,19 +50,23 @@ def scraping():
     return all_info
 
 def find_element(all_info, driver):
-    all_projects = driver.find_elements(By.CSS_SELECTOR, "a[fltrackinglabel='RedirectToPVP']")
-    for projects in all_projects:
-        title = projects.find_element(By.CSS_SELECTOR, 'h2.ng-star-inserted').text
-        data = projects.find_element(By.CSS_SELECTOR,'fl-text.ng-star-inserted').text
+    all_projects = driver.find_elements(By.CSS_SELECTOR, "a[fltrackinglabel=RedirectToPVP]")
+    print(len(all_projects))
+    for project in all_projects:
+        title = project.find_element(By.CSS_SELECTOR, 'fl-heading[class="Title-text"]')
+        print(title.text)
+        data = project.find_element(By.CSS_SELECTOR, "fl-relative-time")
+        print(data.text)
+        link = project.get_attribute('href')
+        print(link)
+        for info in infor:
+            if info in title.text.lower():
+                all_info.append([title.text, data.text, link])
 
-        if "scrap" in title.lower() or "script" in title.lower():
-            all_info.append([title, data])
-            print(title, data)
-        string = data.split()[1]
-        if "day" in string:
+        if data.text.strip() == "1 day":
             return True
-
-    print("-------------------")
+        print(title.text, data.text, link)
+    print("----------------")
     return False
 
 def next_list(driver, wait):
@@ -77,14 +83,11 @@ async def send_to_telegram():
     bot = Bot(token=TOKEN)
     for user in users_id:
         for info in messages:
-            text = f"{info[0]}\n🕒{info[1]}"
+            text = f"{info[0]}\n🕒{info[1]}\n{info[2]}"
             await bot.send_message(chat_id=user, text=text)
             await asyncio.sleep(1)
 
-async def main():
-    scheduler = AsyncIOScheduler()
-    scheduler.add_job(send_to_telegram, 'cron', hour=8, minute=0)
-    scheduler.start()
-    await asyncio.Event().wait()
+
+
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(send_to_telegram())
